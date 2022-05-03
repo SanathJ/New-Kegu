39b72ed23183efc2c8d1825fec66c942c95e38eb
Sanath Jathanna <sanathjathanna@gmail.com>
2022-05-03 16:53:06 +0530

    add: time command

---
+++ commands/time.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Formatters } = require('discord.js');
+const { version } = require('../package.json');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('time')
+		.setDescription('Prints server time and version'),
+	async execute(interaction) {
+		await interaction.reply({ content: Formatters.codeBlock(`Server Time: ${new Intl.DateTimeFormat(['en-US'], { dateStyle: 'long', timeStyle: 'long' }).format(new Date())}\nVersion: ${version}`), ephemeral: true });
+	},
+};
